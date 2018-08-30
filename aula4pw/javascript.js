cfaf668b2be5db3f86d49a2dc4267f1bff7f8c38
OpusNocturn <gabriel.pimentel.sotc@gmail.com>
2018-08-30 20:25:19 -0300

    aula 4 de pw adicionada

---
+++ aula4pw/javascript.js
@@ -0,0 +1,16 @@
+// no caso do "multiplica", pode colocar qualquer palavra, é apenas para você saber o que a função faz.
+
+/*
+function multiplica(a,b){
+	console.log("function multiplica(a,b){...");
+	var resultado = a*b;
+	console.log("imprimindo resultado")
+	return a*b;
+	}
+	var resultadoDaFuncaoMultiplica = multiplica("azul","verde");
+	alert( resultadoDaFuncaoMultiplica );
+*/	
+function divisao(a,b,c,d){
+	console.log("printa alguma coisa");
+	var resultado1 = a/b
+	

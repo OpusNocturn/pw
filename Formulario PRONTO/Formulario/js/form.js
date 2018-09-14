a6b7e979d9ffa89c4bf56e7b376abe111f5f826d
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2018-09-13 22:17:10 -0300

    Formulario atualizado e javascript adicionado

---
+++ Formulario PRONTO/Formulario/js/form.js
@@ -0,0 +1,42 @@
+function validaformulario() {
+			
+		var nome = document.forms["forma1"]["nome"].value;
+		if (nome == ""){
+			alert("O campo 'Nome' não pode ficar em branco.");
+		}
+	
+		
+		var cidade = document.forms["forma1"]["cidade"].value;
+		if(cidade == "") {
+			alert("O campo 'Cidade' não pode ficar em branco.");
+		}
+		
+		
+		var estado = document.forms["forma1"]["estado"].value;
+		if(estado == "") {
+			alert("O campo 'Estado' não pode ficar em branco.")
+		}
+		
+		
+		
+		var uf = document.forms["forma1"]["uf"].value;
+		if (uf == "") {
+			alert("O campo 'UF' não pode ficar em branco.")
+		}
+		
+		
+		
+		var cep = document.forms["forma1"]["cep"].value;
+		if (cep == "") {
+			alert("O campo 'CEP' não pode ficar em branco.")
+		}
+	
+	
+	
+		var pais = document.forms ["forma1"]["pais"].value;
+		if (pais == ""){
+			alert("O campo 'PAÍS' não pode ficar em branco.")
+		}
+		
+		
+}
